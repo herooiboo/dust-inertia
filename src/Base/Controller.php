06fc7b10079b5e1810a765eebb17a5b3ac83ccb5
Manson <mohamed.ibrahim@westore.ai>
2023-06-25 14:49:24 +0300

    Add bindings to controllers

--- src/Base/Controller.php
+++ src/Base/Controller.php
@@ -2,6 +2,7 @@
 
 namespace Dust\Base;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
@@ -9,19 +10,53 @@ use Dust\Base\Contracts\ResponseInterface;
 use Dust\Base\Contracts\RequestHandlerInterface;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 abstract class Controller implements RequestHandlerInterface
 {
+    protected array $bindings = [];
+
     public function __construct(protected ResponseInterface $response, protected Request $request)
     {
     }
 
     public function __invoke(): LengthAwarePaginator|JsonResponse|JsonResource|StreamedResponse|RedirectResponse
     {
+        if ($this->bindings) {
+            $this->resolveBindings();
+        }
+
         return $this->response->send(
             $this,
             $this->request,
         );
+    }
+
+    final protected function resolveBindings(): void
+    {
+        $routeParams = [];
+        $bindings = array_reduce($this->bindings, function($list, $binding) {
+            $list[class_basename($binding)] = $binding;
+            return $list;
+        }, []);
+        foreach ($this->request->route()->parameters as $key => $value) {
+            if (isset($bindings[Str::studly($key)])) {
+                $this->resolve($bindings[Str::studly($key)], $key, $value);
+            }
+        }
+    }
+
+    final public function resolve(string $class, string $key, mixed $value): void
+    {
+        $instance = new $class;
+        $record = $class::where($instance->getRouteKeyName(), $value)->first();
+        if (!$record) {
+            $e = new ModelNotFoundException();
+            $e->setModel($class, [$value]);
+            throw $e;
+        }
+
+        $this->request->route()->setParameter($key, $record);
     }
 }
