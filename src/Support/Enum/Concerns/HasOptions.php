19e4dc51945117239605b7da74808f95ffca5aa2
Manson <mansonx13@gmail.com>
2023-12-29 13:32:44 +0200

    Fix bug for none backed enums

--- src/Support/Enum/Concerns/HasOptions.php
+++ src/Support/Enum/Concerns/HasOptions.php
@@ -3,6 +3,7 @@
 namespace Dust\Support\Enum\Concerns;
 
 use UnitEnum;
+use BackedEnum;
 use Dust\Support\Enum\Contracts\StringableInterface;
 
 /**
@@ -13,7 +14,7 @@ trait HasOptions
     public static function options(): array
     {
         return array_reduce(self::cases(), function ($options, StringableInterface|UnitEnum $case) {
-            $options[] = ['text' => $case->toString(), 'value' => $case->value ?: $case->name];
+            $options[] = ['text' => $case->toString(), 'value' => $this->getValue($case)];
 
             return $options;
         }, []);
@@ -22,9 +23,18 @@ trait HasOptions
     public static function values(): array
     {
         return array_reduce(self::cases(), function ($options, StringableInterface|UnitEnum $case) {
-            $options[] = $case->value ?: $case->name;
+            $options[] = $this->getValue($case);
 
             return $options;
         }, []);
+    }
+
+    public function getValue(UnitEnum|StringableInterface $case): string
+    {
+        if ($case instanceof BackedEnum) {
+            return $case->value ?: $case->name;
+        }
+
+        return $case->name;
     }
 }
